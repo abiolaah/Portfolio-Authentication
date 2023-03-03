4d92bc51facaf03fe7a76ca9bd835283c0ce1530
victoriaajuwon <oluwapelumi.ajuwon@gmail.com>
2023-03-03 18:09:42 -0500

    Fixing contact lsit display issues*

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -64,7 +64,7 @@ module.exports.processLoginPage = (req, res, next) => {
             if (err) {
                 return next(err);
             }
-            return res.redirect('/contactList');
+            return res.redirect('/');
         });
     })(req, res, next);
 }
